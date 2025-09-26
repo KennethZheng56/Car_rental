3e7187daac3a1a376f2a14641f332fe0239cda69
Kenneth_-_Zheng <145678529+KennethZheng56@users.noreply.github.com>
2025-09-26 17:54:55 -0400

    Update Temp_database.py

--- Temp_database.py
+++ Temp_database.py
@@ -1,88 +1,14 @@
 from pyscript import document, when
-from js import window
-import json
 
-DB_KEY_USERS = "temp_users"
-DB_KEY_CARS = "temp_cars"
-
-DEFAULT_USERS = [
-    {"username": "Matthew_Liao", "password": "IS_GAY", "role": "admin"},
-    {"username": "Homer", "password": "Wang", "role": "user"}
-]
-
-DEFAULT_CARS = ["Toyota", "Honda", "BMW"]
-
-def load_cars():
-    cars_json = window.localStorage.getItem(DB_KEY_CARS)
-    if cars_json is None:
-        save_cars(DEFAULT_CARS)
-        return DEFAULT_CARS
-    return json.loads(cars_json)
-
-def save_cars(cars):
-    window.localStorage.setItem(DB_KEY_CARS, json.dumps(cars))
-
-def add_car(car):
-    cars = load_cars()
-    if car not in cars:
-        cars.append(car)
-        save_cars(cars)
-
-def remove_car(car):
-    cars = load_cars()
-    if car in cars:
-        cars.remove(car)
-        save_cars(cars)
-
-cars = load_cars()
+cars = ["Toyota", "Honda", "BMW"]
 
 @when("click", "#view-catalogue")
-def show_cars(event=None):
+def hi(event=None):
+    # Build an unordered list of cars
     html = "<ul>"
     for car in cars:
         html += f"<li>{car}</li>"
     html += "</ul>"
+
+    # Insert into the <p id="test"> element
     document.getElementById("test").innerHTML = html
-
-def load_db():
-    users_json = window.localStorage.getItem(DB_KEY_USERS)
-    if users_json is None:
-        save_db(DEFAULT_USERS)
-        return DEFAULT_USERS
-    return json.loads(users_json)
-
-def save_db(users):
-    window.localStorage.setItem(DB_KEY_USERS, json.dumps(users))
-
-def get_users():
-    return load_db()
-
-def add_user(username, password, role="user"):
-    users = load_db()
-    if any(u["username"] == username for u in users):
-        return False
-    users.append({"username": username, "password": password, "role": role})
-    save_db(users)
-    return True
-
-def remove_user(username):
-    users = load_db()
-    users = [u for u in users if u["username"] != username]
-    save_db(users)
-    return True
-
-def change_role(username, role):
-    users = load_db()
-    for u in users:
-        if u["username"] == username:
-            u["role"] = role
-            save_db(users)
-            return True
-    return False
-
-def verify_user(username, password):
-    users = load_db()
-    for u in users:
-        if u["username"] == username and u["password"] == password:
-            return u
-    return None
