935a9f2fd98f36aed4f7ec8d9575ac0c6debdb62
Kenneth_-_Zheng <145678529+KennethZheng56@users.noreply.github.com>
2025-09-26 15:21:58 -0400

    Create Temp_database.py

---
+++ Temp_database.py
@@ -0,0 +1,6 @@
+login = [{"username":"Matthew_Liao", "password":"IS_GAY"}]
+cars = []
+def add_user(user:str, password:str):
+  login.append({"username":user, "password":password})
+
+def check_user()
