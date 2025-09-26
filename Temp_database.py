79c3ec47221c24e29ac316d8b7b83dcc61547827
Kenneth_-_Zheng <kennethzheng56@gmail.com>
2025-09-26 16:48:41 -0400

    test

--- Temp_database.py
+++ Temp_database.py
@@ -1,7 +1,10 @@
+from pyscript import document
+
 login = [{"username":"Matthew_Liao", "password":"IS_GAY"}]
-cars = [{"car", "make", "model"}]
+cars = [{"car":"Honda", "make":"wow", "model":"no", "year":"old"}]
 
 def add_user(user:str, password:str):
   login.append({"username":user, "password":password})
 
-def check_user()
+def hi():
+  document.getElementById("test").innerText = str(cars)
