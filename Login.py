b73928cc457071e64cb19ed05fc6c8cfc1251127
Eddie Chen <143721672+SseltnelaT@users.noreply.github.com>
2025-09-26 14:50:40 -0400

    Create Login.py

---
+++ Login.py
@@ -0,0 +1,14 @@
+class Login:
+  accounts = {}
+
+  def Check(name:str, password: str):
+    if (account{name} == password):
+      #access the account
+    else:
+      return "wrong login info"
+
+  def AddAccount():
+    
+      
+
+  
