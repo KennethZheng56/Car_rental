2f20e39964bd5cf50e6b6f8c16aacfd4fd3c3217
MLiao003 <145688713+MLiao003@users.noreply.github.com>
2025-09-26 15:22:19 -0400

    Update Customer.py

--- Customer.py
+++ Customer.py
@@ -1,7 +1,11 @@
 class Customer:
   Username = ""
-  
+  password = ""
 
+  def __init__(self, Username, password):
+    self.Username = Username
+    self.password = password
+    
   def Pay():
 
   def SelectVehicle():
